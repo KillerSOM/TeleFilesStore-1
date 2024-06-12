6fdab6be01a97c545cc329b1bdaa1e75cde89bcc
KillerSOM <136156372+KillerSOM@users.noreply.github.com>
2024-06-12 18:25:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ from pyrogram.types import (
     CallbackQuery,
     Message
 )
-from configs import Config
+from configs import config
 from handlers.database import db
 from handlers.add_user_to_db import add_user_to_database
 from handlers.send_file import send_media_and_reply
