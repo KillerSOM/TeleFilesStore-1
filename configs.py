c71ddd614a694909c7fc0f7d74b75e04a13ff90b
KillerSOM <136156372+KillerSOM@users.noreply.github.com>
2024-06-12 18:40:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,4 +18,4 @@ class Config(object):
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_BOT_TEXT = f"""
+	ABOUT_BOT_TEXT = f""I am a File-Store Bot."
