752dbdbb48a54dd77f4dbd29642190918bcb12b6
KillerSOM <136156372+KillerSOM@users.noreply.github.com>
2024-06-13 00:03:24 +0530

    Update database.py

--- handlers/database.py
+++ handlers/database.py
@@ -1,18 +1,79 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.database import Database as PyMongoDatabase
+# (c) @AbirHasan2005
+
+import datetime
+import motor.motor_asyncio
+from configs import Config
+
 
 class Database:
-    def __init__(self, database_url: str, database_name: str):
-        if not isinstance(database_url, str) or not isinstance(database_name, str):
-            raise TypeError("database_url and database_name must be instances of str")
 
-        self._client = AsyncIOMotorClient(database_url)
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
+        self.col = self.db.users
 
-# Usage example:
-from configs.config import Config
+    def new_user(self, id):
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason=''
+            )
+        )
 
-try:
-    db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
-except TypeError as e:
-    print(f"Configuration error: {e}")
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason):
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('ban_status', default)
+
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({'ban_status.is_banned': True})
+        return banned_users
+
+
+db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
